7a639b0c206b93d57f6aa0438f7bed6fa8f60981
Alan Leszczuk <alan.leszczuk@infermedica.com>
2021-11-18 21:41:05 +0100

    both counters with implemented events - increment and decrement

--- svelteapp/src/components/Counter.svelte
+++ svelteapp/src/components/Counter.svelte
@@ -1,10 +1,18 @@
 <script>
   let count = 0;
+  const increment = () => count++;
+  const decrement = () => count--;
 </script>
 
-<div class="flex items-center justify-around m-auto max-w-sm pt-2">
-	<button class="px-4 py-2 text-sm text-yellow-800 font-semibold rounded-xl border border-yellow-400 hover:text-white hover:bg-yellow-800 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2"> DECREMENT </button>
-	<h3>{ count }</h3>
-	<button class="px-4 py-2 text-sm text-yellow-800 font-semibold rounded-xl border border-yellow-400 hover:text-white hover:bg-yellow-800 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2"> INCREMENT </button>
+<div class="flex items-center justify-between m-auto max-w-sm mt-5 px-5">
+	<button on:click={decrement}
+					class="px-4 py-2 text-sm text-yellow-800 font-semibold rounded-xl border border-yellow-400 hover:text-white hover:bg-yellow-800 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2">
+		DECREMENT
+	</button>
+	<h3 class="tracking-widest">{ count }</h3>
+	<button on:click={increment}
+					class="px-4 py-2 text-sm text-yellow-800 font-semibold rounded-xl border border-yellow-400 hover:text-white hover:bg-yellow-800 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-offset-2">
+		INCREMENT
+	</button>
 </div>
 
--- vueapp/src/components/Counter.vue
+++ vueapp/src/components/Counter.vue
@@ -2,12 +2,20 @@
 import {ref} from 'vue';
 
 const count = ref(0);
+const increment = () => count.value++;
+const decrement = () => count.value--;
 </script>
 
 <template>
-	<div class="flex items-center justify-around m-auto max-w-sm pt-2">
-		<button class="px-4 py-2 text-sm text-green-800 font-semibold rounded-xl border border-green-400 hover:text-white hover:bg-green-800 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2"> DECREMENT </button>
-		<h3>{{ count }}</h3>
-		<button class="px-4 py-2 text-sm text-green-800 font-semibold rounded-xl border border-green-400 hover:text-white hover:bg-green-800 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2"> INCREMENT </button>
+	<div class="flex items-center justify-between m-auto max-w-sm mt-5 px-5">
+		<button @click="decrement"
+		        class="px-4 py-2 text-sm text-green-800 font-semibold rounded-xl border border-green-400 hover:text-white hover:bg-green-800 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2">
+			DECREMENT
+		</button>
+		<h3 class="tracking-widest">{{ count }}</h3>
+		<button @click="increment"
+		        class="px-4 py-2 text-sm text-green-800 font-semibold rounded-xl border border-green-400 hover:text-white hover:bg-green-800 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2">
+			INCREMENT
+		</button>
 	</div>
 </template>
