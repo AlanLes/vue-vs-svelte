8735c5545e806a5bf0a8f095c7f6e53aef665cf6
Alan Leszczuk <alan.leszczuk@infermedica.com>
2021-11-18 21:32:06 +0100

    counter.vue and counter.svelte both are the same

---
+++ svelteapp/src/components/Counter.svelte
@@ -0,0 +1,19 @@
+<script>
+  let count = 0;
+</script>
+
+<div class="counter">
+	<button> DECREMENT </button>
+	<h3>{ count }</h3>
+	<button> INCREMENT </button>
+</div>
+
+<style>
+    .counter {
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        margin: auto;
+        max-width: 250px;
+    }
+</style>
---
+++ vueapp/src/components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="counter">
+		<button> DECREMENT </button>
+		<h3>{{ count }}</h3>
+		<button> INCREMENT </button>
+	</div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const count = ref(0);
+</script>
+
+<style>
+.counter {
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	margin: auto;
+	max-width: 250px;
+}
+</style>
